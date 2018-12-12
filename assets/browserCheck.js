6294d936b67de14e5b0086d5f978b4a93fcd0296
Piotr Francug <piotr.francug@gmail.com>
2018-12-12 14:48:02 +0100

    Wyłączono wsparcie dla IE

---
+++ assets/browserCheck.js
@@ -0,0 +1,33 @@
+const ua = window.navigator.userAgent
+
+function detectIE() {
+	const msie = ua.indexOf('MSIE ')
+	if (msie > 0) {
+		return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10)
+	}
+	const trident = ua.indexOf('Trident/')
+	if (trident > 0) {
+		const rv = ua.indexOf('rv:')
+		return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10)
+	}
+	const edge = ua.indexOf('Edge/')
+	if (edge > 0) {
+		return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10)
+	}
+	return false
+}
+
+const version = detectIE()
+
+const pushAlert = function (version) {
+	document.querySelector('#main').innerHTML =
+		'<div class="jumbotron col-12 text-center">' +
+		'<p class="h3 brand">Twoja przęglądarka "' + version + '" nie jest wspierana :(</p>' +
+		'<samp class="">Sugerowane przeglądarki: Chrome / Firefox</samp></div>'
+}
+
+if (version >= 12) {
+	pushAlert('Edge ' + version)
+} else if (version > 0) {
+	pushAlert('IE ' + version)
+}
