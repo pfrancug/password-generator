9911778cc7c4a62f2a04031f0bac91426a3407d4
Piotr Francug <A21411@ka.policja.gov.pl>
2019-09-30 09:42:12 +0200

    Fix: IE support for pokemon package

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.js$/,
-            exclude: /node_modules/,
+            exclude: /node_modules\/(?!(pokemon|unique-random|unique-random-array)\/).*/,
             use: {
                 loader: 'babel-loader',
                 options: {
